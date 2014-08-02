7e9ae8a367605b778a2c5b3d8085dad971f83d85
Fosco Marotto <gfosco@gmail.com>
2014-08-02 12:59:50 -0700

    Updates

--- rotor-server/app.js
+++ rotor-server/app.js
@@ -2,7 +2,34 @@
 
 var http = require('http');
 var caller = require('request');
-var buffet = require('buffet');
+var router = require('choreographer').router();
+var socket = require('socket.io');
+var url = require('url');
+var buffet = require('buffet')({
+  root:'./',
+  poweredBy:false
+});
 
-console.log('hi');
+var Parse = require('parse').Parse;
 
+var Clients = [];
+var Identity = [];
+
+
+router.on('/', function(req, res) {
+  res.end('Hi');
+});
+
+
+
+var app = http.createServer(router);
+var io = socket.listen(app);
+app.listen(80);
+
+var connectedClients = 0;
+
+io.sockets.on('connection', function(socket) {
+  Clients[socket.id] = socket;
+  connectedClients++;
+
+});
