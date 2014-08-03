1d04b850382a6ee8516ba601f887331308315530
Fosco Marotto <gfosco@gmail.com>
2014-08-02 20:34:00 -0700

    login screen

--- login-screen/index.js
+++ login-screen/index.js
@@ -5,23 +5,27 @@ $(document).ready(function() {
             $('#login').fadeOut();
             $('#loading').fadeIn();      
         }
-    });
 
-    $.ajax({
-        type: 'GET',
-        url: '',
-        dataType: 'jsonp',
-        success: function(response){
-            console.log(response);
-            $("#success").fadeIn();
-            $('body').css("background-color", "#7ed21c");
-        },
-        error: function(response){
-            console.log(response);
-        }.done(function() {
-            // $("#success").fadeIn();
-            // $('body').css("background-color", "#7ed21c");
-        })
+        $.ajax({
+            type: 'GET',
+            url: 'http://rtrp.io/client/AuthRotor',
+            dataType: 'json',
+            success: function(response) {
+                console.log(response);
+                if (response == "true") {
+                    $("#success").fadeIn();
+                    $('body').css("background-color", "#7ed21c");
+                } else {
+                    $('#loading').fadeOut();
+                    $('#login').fadeIn();
+                }
+            },
+            error: function(response){
+                console.log(response);
+                $('#loading').fadeOut();
+                $('#login').fadeIn();
+            }
+        });
     });
 
 });
